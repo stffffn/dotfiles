69faf64535e858d6e039d2eae022e60a2bfac620
Steffen Weitz <weitz.steffen@gmail.com>
2022-02-11 22:26:17 +0100

    Update gitignore and vs code settings and extensions

--- vscode.sh
+++ vscode.sh
@@ -1,4 +1,5 @@
 code --install-extension Angular.ng-template
+code --install-extension bradlc.vscode-tailwindcss
 code --install-extension dracula-theme.theme-dracula
 code --install-extension esbenp.prettier-vscode
 code --install-extension PKief.material-icon-theme
