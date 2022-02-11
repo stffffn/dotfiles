f41e0e29a5a077c41edecbae6925bc3e21760402
Steffen Weitz <weitz.steffen@gmail.com>
2022-02-11 16:40:10 +0100

    Update brewfile and vs code extensions

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,4 @@
+code --install-extension Angular.ng-template
 code --install-extension dracula-theme.theme-dracula
 code --install-extension esbenp.prettier-vscode
 code --install-extension PKief.material-icon-theme
