0f64ad780d29210f089a2a5a7d5a79a394c999e6
Steffen Weitz <weitz.steffen@gmail.com>
2022-11-28 19:45:14 +0100

    Update brewfile

--- vscode.sh
+++ vscode.sh
@@ -2,7 +2,8 @@ code --install-extension Angular.ng-template
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension dracula-theme.theme-dracula
 code --install-extension esbenp.prettier-vscode
+code --install-extension GraphQL.vscode-graphql
+code --install-extension GraphQL.vscode-graphql-execution
+code --install-extension GraphQL.vscode-graphql-syntax
 code --install-extension PKief.material-icon-theme
-code --install-extension ritwickdey.LiveServer
-code --install-extension samplavigne.p5-vscode
 code --install-extension svelte.svelte-vscode
